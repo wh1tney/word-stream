e7be4ee95daa50615e432bd79ad990742b887cdd
Germain Boue & Kevin Kang <pair+boue.kevinkang88@devbootcamp.com>
2014-07-19 14:02:16 -0700

    Pseudocoding - self explanatory see for details

---
+++ public/js/text_streaming.js
@@ -0,0 +1,10 @@
+// Pseudocode
+// binding an event of streaming to the button "begin"
+// run the algorithm that goes thru each letter and changes colors
+// fixed duration will be the same for every "letter" or "word" (not mvp)
+// Input: A string as text which we would need to split in terms of letters or words.
+
+// Step 1: to take the string that is given to us by the parsing process and turn that into an array that contains strings
+// Step 2: iterate through each element and wrap them around span tags with a certain color in order to add to the DOM
+// Step 3: Store step 2 as a variable and add to the DOM (html page)
+// Step 4: Actually create a function that colors in each letter or word
