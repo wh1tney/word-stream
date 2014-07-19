877d57cc83c7e3264a706713be3cdbb46ed87ec8
Allison Wong & Yen Tran <pair+azywong.yenhtran@devbootcamp.com>
2014-07-19 14:16:21 -0700

    setup stories CRUD

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,31 +1,33 @@
 get "/" do
-  # redirect to /stories
   redirect "/stories"
 end
 
 get "/stories" do
-  # get list of all stories
-end
-
-get "/stories/:id" do
-  # get a specific story
-  # also read the story
-end
-
-post "/stories" do
-  # new story
+  @stories = Story.all
+  erb :stories
 end
 
 get "/stories/new" do
-  # form for creating new story
-end
-
-get "/stories/:id/edit" do
-  # form for editing story
+  erb :new
 end
 
 put "/stories/update" do
   # update story
+end
+
+get "/stories/:id" do
+  @story = Story.find(params[:id])
+  # also read the story
+  erb :story
+end
+
+post "/stories" do
+  story = Story.create(params)
+  redirect "stories/#{story.id}"
+end
+
+get "/stories/:id/edit" do
+  # form for editing story
 end
 
 delete "/stories/:id" do
