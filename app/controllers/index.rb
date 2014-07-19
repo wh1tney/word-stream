058e3b40200e09c84f9c9ed59aac9f89a926e19f
allison <azywong@gmail.com>
2014-07-19 12:25:33 -0700

    added routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,33 @@
 get "/" do
-  @variable = "hello"
-  erb :index
+  # redirect to /stories
+  redirect "/stories"
 end
 
-post "/url" do
-  #do something
-  redirect "/"
+get "/stories" do
+  # get list of all stories
+end
+
+get "/stories/:id" do
+  # get a specific story
+  # also read the story
+end
+
+post "/stories" do
+  # new story
+end
+
+get "/stories/new" do
+  # form for creating new story
+end
+
+get "/stories/:id/edit" do
+  # form for editing story
+end
+
+put "/stories/update" do
+  # update story
+end
+
+delete "/stories/:id" do
+  #delete story
 end
