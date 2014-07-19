1291fb3db759ec0bf7d657d37b1a7d58ef6d1565
Germain Boue & Kevin Kang <pair+boue.kevinkang88@devbootcamp.com>
2014-07-19 14:26:59 -0700

    The objective of this commit was to implement a function to format the story and then as next step would be a function to iterate and change each letters color
      - We are splitting the string into string elements that consist of one letter in an array
      - We iterate and convert into span ids with light grey color

--- public/js/text_streaming.js
+++ public/js/text_streaming.js
@@ -8,3 +8,20 @@
 // Step 2: iterate through each element and wrap them around span tags with a certain color in order to add to the DOM
 // Step 3: Store step 2 as a variable and add to the DOM (html page)
 // Step 4: Actually create a function that colors in each letter or word
+
+function formating_story(story){
+  var text = "";
+  // splits into an array of letters such as ["a", "b", "c"]
+  var array_of_letters = text.split("") ;
+  $.each(array_of_letters, function(a, b) {
+    // a is the index, b is the element
+    text += "<span id='p" + a + " style='color:#C0C0C0'>" + b[0] + "</span>";
+  })
+};
+
+ $(document).ready(function(){
+
+  $('#startBtn').on('click',function(){
+
+  });
+});
