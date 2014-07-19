3b826de3cb5789e38de2cdcfb6df9d1f4df22b2e
Jon Pabico & Whitney O'Banner <pair+jpabico.wh1tney@devbootcamp.com>
2014-07-19 14:23:47 -0700

    Change data type for story from string to text

--- db/migrate/20140719121315_create_stories.rb
+++ db/migrate/20140719121315_create_stories.rb
@@ -2,7 +2,7 @@ class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
       t.string :title
-      t.string :story
+      t.text :story
       t.timestamps
     end
   end
